ecc596d9c28d207113f627da4a4cdf1d53626d0f
joshforcier <joshua.forcier@decopac.com>
2020-01-23 15:37:07 -0600

    ui updates

--- src/components/Hotel.vue
+++ src/components/Hotel.vue
@@ -2,26 +2,27 @@
     <div class="content text_align_center hotel">
         <div class="mx-3 text-center">
             <h1 class="text_align_center">Hotel</h1>
+            <div class="hotel__container">
+                <h3 class="text_align_center">TownePlace Suites</h3>
+                <img
+                    src="../assets/exterior.jpg"
+                    alt="hotel"
+                    class="hotelImg"
+                >
+                <div>
+                    2706 Pontiac Place<br />
+                    Janesville, WI 53545<br />
+                    (608) 898-4200
+                </div>
+                <a href="https://www.marriott.com/event-reservations/reservation-link.mi?id=1579538297375&key=GRP&app=resvlink">Hotel Reservation</a><br /><br />
 
-            <h3 class="text_align_center">TownePlace Suites</h3>
-            <img
-                src="../assets/exterior.jpg"
-                alt="hotel"
-                class="hotelImg"
-            >
-            <div>
-                2706 Pontiac Place<br />
-                Janesville, WI 53545<br />
-                (608) 898-4200
+                <p class="hotelInfo">
+                        For your convenience, a block of rooms has been reserved at TownePlace Suites in Janesville. 
+                    When calling to make your reservation, please mention the Forcier/Meyers wedding in order to receive a discounted rate. 
+                    Please follow the link above or call 608-898-4200 to make your reservation.
+                </p>
+                <p class="hotelInfo info_date">Be sure to book your hotel room by August 21, 2020 in order to receive the discount.</p>
             </div>
-            <a href="https://www.marriott.com/event-reservations/reservation-link.mi?id=1579538297375&key=GRP&app=resvlink">Hotel Reservation</a><br /><br />
-
-            <p class="hotelInfo">
-                    For your convenience, a block of rooms has been reserved at TownePlace Suites in Janesville. 
-                When calling to make your reservation, please mention the Forcier/Meyers wedding in order to receive a discounted rate. 
-                Please follow the link above or call 608-898-4200 to make your reservation.
-            </p>
-            <p class="hotelInfo info_date">Be sure to book your hotel room by August 21, 2020 in order to receive the discount.</p>
         </div>
     </div>
 </template>
@@ -42,8 +43,17 @@ export default {
     }
 }
 
+.hotel__container {
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1), 0 3px 6px 0 rgba(0, 0, 0, 0.09);
+    border-radius: 10px;
+    margin-top: 20px;
+    background-color: white;
+    padding: 20px;
+}
+
 .hotelImg {
     width: 100%;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1), 0 3px 6px 0 rgba(0, 0, 0, 0.09);
 }
 
 .hotelInfo {
